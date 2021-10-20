101562393d7dde558f85466e748adbc8dda405df
Shabanov Roman <shabanov.roman0@gmail.com>
2021-10-20 10:56:58 +0800

    Removed MQTT

--- SerialLed/led-manager/src/main.rs
+++ SerialLed/led-manager/src/main.rs
@@ -1,13 +1,42 @@
-use paho_mqtt::errors::Result;
-use led_manager::set_fire;
+use serial::unix;
+use rand::Rng;
+use std::{
+    thread,
+    time,
+    path::Path,
+    io::{Write}
+};
+use anyhow::{Context, Result};
+
+const LED_NUM: usize = 12;
+const CHANNELS_NUM: usize = LED_NUM * 3;
 
 fn main() -> Result<()> {
 
     let board_port = std::env::args()
         .nth(1)
-        .expect("no port given");
+        .with_context(|| format!("Failed to run app: no port was given"))?;
 
-    set_fire(&board_port)
+    if let Err(error) = set_fire(&board_port) {
+        return Err(error.into())
+    }
+
+    Ok(())
 }
 
+fn set_fire(board_port: &str) -> Result<()> {
+    let mut port = unix::TTYPort::open(Path::new(&board_port))?;
 
+    let mut rgbs: Vec<u8> = vec![0; CHANNELS_NUM];
+    let mut iteration: usize = 1;
+
+    loop {
+        for i in 0..iteration {
+            rgbs[i] = rand::thread_rng().gen_range(0..255)
+        }
+        port.write(&rgbs[..iteration])?;
+        iteration = (iteration + 1) % CHANNELS_NUM;
+
+        thread::sleep(time::Duration::from_millis(100));
+    }
+}
--- SerialLed/led-manager/src/mqtt/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod mqtt_publisher;
-pub mod mqtt_subscriber;
-
-pub use mqtt_publisher::MqttPubliser;
-pub use mqtt_subscriber::MqttSubscriber;
